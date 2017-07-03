ba65ed3530d3e6fa90783084c143d065f5a02411
mateoszym <mateoszym@gmail.com>
2017-07-03 17:46:27 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+var a = 3;
+var h = 6;
+var triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
